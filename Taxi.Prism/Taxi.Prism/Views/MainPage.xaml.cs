ad3b3a06a60ef5808c80cf3fe82d13e59334de64
Sebastian <mobilfit1@gmail.com>
2021-04-09 15:07:05 -0400

    Implementacion de taxis controller

---
+++ Taxi.Prism/Taxi.Prism/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Taxi.Prism.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Taxi.Web/Data/Entities/TaxiEntity.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Taxi.Web.Data.Entities
+{
+    public class TaxiEntity
+    {
+        public int ID { get; set; }
+
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The Field must have {1} characteres.")]
+        [Required(ErrorMessage = "The Field {0} is mandatory.")]
+        public string Plaque { get; set; }
+    }
+}
