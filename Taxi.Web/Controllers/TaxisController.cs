93c030b620d5494269cae57dbf1b01e6d2e5e51d
Sebastian <mobilfit1@gmail.com>
2021-04-09 18:26:07 -0400

    AddUniqueIndexvalidation

--- Taxi.Web/Controllers/TaxisController.cs
+++ Taxi.Web/Controllers/TaxisController.cs
@@ -60,8 +60,23 @@ namespace Taxi.Web.Controllers
             {
                 taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
                 _context.Add(taxiEntity);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Already exists a taxi with the same plaque.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+                    }
+
+                }
             }
             return View(taxiEntity);
         }
@@ -98,9 +113,23 @@ namespace Taxi.Web.Controllers
             {
                 taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
                 _context.Update(taxiEntity);
-                await _context.SaveChangesAsync();
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Already exists a taxi with the same plaque.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+                    }
 
-                return RedirectToAction(nameof(Index));
+                }
             }
             return View(taxiEntity);
         }
---
+++ Taxi.Web/Data/SeedDb.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _dataContext;
+
+        public SeedDb(DataContext dataContext)
+        {
+            this._dataContext = dataContext;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _dataContext.Database.EnsureCreatedAsync();
+            await CheckTaxisAsync();
+        }
+
+        private async Task CheckTaxisAsync()
+        {
+            if (_dataContext.Taxis.Any())
+            {
+                return;
+            }
+
+            _dataContext.Taxis.Add(new TaxiEntity()
+            {
+                Plaque = "AAER45",
+                Trips = new List<TripEntity>
+                {
+                    new TripEntity
+                    {
+                        StartDate = DateTime.UtcNow,
+                        EndDate = DateTime.UtcNow.AddMinutes(30),
+                        Qualification = 4.5f,
+                        Source = "ITM Fraternidad",
+                        Target = "ITM Robledo",
+                        Remarks = "Buen servicio"
+                    },
+                    new TripEntity
+                    {
+                        StartDate = DateTime.UtcNow,
+                        EndDate = DateTime.UtcNow.AddMinutes(30),
+                        Qualification = 4.2f,
+                        Source = "ITM direccion",
+                        Target = "ITM llego",
+                        Remarks = "Mas o menos servicio"
+                    }
+                }
+            });
+            _dataContext.Taxis.Add(new TaxiEntity()
+            {
+                Plaque = "TQEP99",
+                Trips = new List<TripEntity>
+                {
+                    new TripEntity
+                    {
+                        StartDate = DateTime.UtcNow,
+                        EndDate = DateTime.UtcNow.AddMinutes(30),
+                        Qualification = 4.5f,
+                        Source = "ITM Fraternidad",
+                        Target = "ITM Robledo",
+                        Remarks = "Buen servicio"
+                    },
+                    new TripEntity
+                    {
+                        StartDate = DateTime.UtcNow,
+                        EndDate = DateTime.UtcNow.AddMinutes(30),
+                        Qualification = 4.2f,
+                        Source = "ITM direccion",
+                        Target = "ITM llego",
+                        Remarks = "Mas o menos servicio"
+                    }
+                }
+            });
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
